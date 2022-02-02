95651b3cc856bf281a6f16a70ba0642f92daf74b
s1kebtz <adacenkoboos@gmail.com>
2022-02-02 09:57:14 +0700

    media files

--- beats/models.py
+++ beats/models.py
@@ -12,7 +12,7 @@ class Beat(models.Model):
     name = models.CharField(max_length=50, help_text='Type beat name.')
     bpm = models.SmallIntegerField(help_text='Type BPM for the beat.')
     artist = models.ManyToManyField(Artist, help_text='Select Artists for the beat')
-    wrap = models.ImageField(upload_to='wraps/', default='images/wraps/S.svg')
+    wrap = models.ImageField(upload_to='wraps/', default='wraps/S.svg')
     wave = models.FileField(upload_to='audio/wave/', default='')
     mp3 = models.FileField(upload_to='audio/mp3/', default='')
     def __str__(self):
